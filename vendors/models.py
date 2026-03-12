abe4c4f7e4b4d449511b82afce89e4df5d056303
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-12 11:24:47 +0500

    added vendor models

--- vendors/models.py
+++ vendors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Vendor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='vendor_profile')
+    store_name = models.CharField(max_length=255)
+    store_description = models.TextField(blank=True, null=True)
+    store_logo = models.ImageField(upload_to='vendor_logos/', blank=True, null=True)
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.store_name
