882f84b24d7fe85f7144ccbd8c2fea530f346717
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-12 13:23:58 +0500

    products un admin written

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Product, ProductImage, Category
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'vendor', 'price', 'stock', 'category', 'created_at')
+    list_filter = ('vendor', 'category', 'created_at')
+    search_fields = ('name', 'description')
+    ordering = ('-created_at',) 
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('product', 'alt_text', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('alt_text',)
+    ordering = ('-created_at',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):      
+    list_display = ('name', 'slug', 'parent', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'slug')
+    ordering = ('-created_at',)
+    
