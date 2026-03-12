8ef9b9030ab05e1bc19912d5a9ad1cb5488fac41
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-12 12:19:39 +0500

    ProductImage model added

--- products/models.py
+++ products/models.py
@@ -18,3 +18,11 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='product_images/')
+    alt_text = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
