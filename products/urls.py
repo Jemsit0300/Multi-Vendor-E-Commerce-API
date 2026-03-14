940f7bdd3621024d0e77f1bc6ce83881b4a230fb
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-14 12:57:14 +0500

    added category

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProductCreateView
+from .views import CategoryView, ProductCreateView
+
 
 urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='product-create'),
+    path('category/', CategoryView.as_view(), name='category-create'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
+from .models import Category
 from vendors.permissions import IsApprovedVendor
-from .serializers import ProductSerializer
+from .serializers import CategorySerializer, ProductSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 class ProductCreateView(APIView):
     permission_classes = [IsApprovedVendor]
@@ -12,3 +14,7 @@ class ProductCreateView(APIView):
             serializer.save(vendor=request.user)
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+    
+class CategoryView(generics.CreateAPIView): 
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
