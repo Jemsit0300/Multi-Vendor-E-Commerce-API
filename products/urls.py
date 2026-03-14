ae66a9ffb8ee08a04e670370ca1e4f272c9cf23c
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-14 13:04:49 +0500

    Category added

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-from .views import CategoryView, ProductCreateView
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoryViewSet, ProductCreateView
+
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet, basename='categories')
 
 
 urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='product-create'),
-    path('category/', CategoryView.as_view(), name='category-create'),
+    path('', include(router.urls)),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,12 @@
-from .models import Category
-from vendors.permissions import IsApprovedVendor
-from .serializers import CategorySerializer, ProductSerializer
-from rest_framework.views import APIView
+from rest_framework import permissions, viewsets
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework.views import APIView
+
+from vendors.permissions import IsApprovedVendor
+
+from .models import Category
+from .serializers import CategorySerializer, ProductSerializer
+
 
 class ProductCreateView(APIView):
     permission_classes = [IsApprovedVendor]
@@ -14,7 +17,28 @@ class ProductCreateView(APIView):
             serializer.save(vendor=request.user)
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
-    
-class CategoryView(generics.CreateAPIView): 
+
+
+class CategoryPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        action = getattr(view, "action", None)
+
+        if action in ["list", "retrieve"]:
+            return True
+
+        if not request.user or not request.user.is_authenticated:
+            return False
+
+        if action == "create":
+            return request.user.is_staff or request.user.is_superuser
+
+        if action in ["update", "partial_update", "destroy"]:
+            return request.user.is_superuser
+
+        return False
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [CategoryPermission]
