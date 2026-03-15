de1833d73daec2bc3cb7c6e017de8e174ae1cfa5
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-15 18:03:12 +0500

    Product Image Upload

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, ProductViewSet
+from .views import CategoryViewSet, ProductImageDeleteView, ProductImageView, ProductViewSet
 from django.conf.urls.static import static
 
 
@@ -13,6 +13,17 @@ router.register(r'products', ProductViewSet, basename='products')
 
 urlpatterns = [
     path('', include(router.urls)),
+   
+    path(
+        "api/products/<int:product_id>/images/",
+        ProductImageView.as_view(),
+    ),
+   
+    path(
+        "api/products/<int:product_id>/images/<int:id>/",
+        ProductImageDeleteView.as_view(),
+    ),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    
