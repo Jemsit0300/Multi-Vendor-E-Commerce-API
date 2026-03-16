2c25925da7d0fcaa1945ef81a2f94729c07b26b9
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-16 21:31:42 +0500

    fixed models

--- orders/models.py
+++ orders/models.py
@@ -1,16 +1,22 @@
 from django.db import models
-from users.views import User
+from django.contrib.auth import get_user_model
 from products.models import Product
 from vendors.models import Vendor
 
+User = get_user_model()
+
 class Order(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "Pending"),
+        ("paid", "Paid"),
+        ("processing", "Processing"),
+        ("shipped", "Shipped"),
+        ("delivered", "Delivered"),
+    )
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(choices=[
-        ('pending','Pending'),
-        ('shipped','Shipped'),
-        ('delivered','Delivered')
-        ], default='pending')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
