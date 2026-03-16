f8e9c45bb49f73c1b8b7c0785f7b43726a49fecc
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-16 20:45:31 +0500

    The user model was done correctly.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer
+
 User = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
