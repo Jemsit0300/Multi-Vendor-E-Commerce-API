e7b3ad6c20ee13c9d4e5747914e20e5c6b5ab6f4
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-16 19:22:07 +0500

    A cartitem model was created.

--- cart/models.py
+++ cart/models.py
@@ -6,3 +6,7 @@ from products.models import Product
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
