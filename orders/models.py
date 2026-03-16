854142408d00cf8cbb13230160ac22590b31e44a
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-16 20:28:07 +0500

    fixed

--- orders/models.py
+++ orders/models.py
@@ -22,3 +22,6 @@ class OrderItem(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
