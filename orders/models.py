a60b118bb958289cb65d49706a8037e1f0e39d25
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-16 20:27:03 +0500

    fixed

--- orders/models.py
+++ orders/models.py
@@ -6,7 +6,11 @@ from vendors.models import Vendor
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(choices=[('pending','Pending'),('shipped','Shipped'),('delivered','Delivered')], default='pending')
+    status = models.CharField(choices=[
+        ('pending','Pending'),
+        ('shipped','Shipped'),
+        ('delivered','Delivered')
+        ], default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
 
 class OrderItem(models.Model):
