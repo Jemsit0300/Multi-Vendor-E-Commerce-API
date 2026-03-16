b122ed443dcc72bd4cb05d49d7fc846b587f2699
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-16 10:31:33 +0500

    fixed

---
+++ orders/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Order
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'total_price', 'status', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
