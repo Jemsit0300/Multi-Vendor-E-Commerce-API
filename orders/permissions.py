3e5cd611d7b2a0ab928f16f770658f637a2e18b2
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-16 20:32:19 +0500

    fixed

--- orders/permissions.py
+++ orders/permissions.py
@@ -5,15 +5,12 @@ class IsOwnerVendorOrAdmin(BasePermission):
 
     def has_object_permission(self, request, view, obj):
 
-        # Admin
         if request.user.is_staff:
             return True
 
-        # Customer
         if obj.user == request.user:
             return True
 
-        # Vendor
         for item in obj.items.all():
             if item.product.vendor == request.user:
                 return True
