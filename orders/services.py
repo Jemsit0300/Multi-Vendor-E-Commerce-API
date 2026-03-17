3c4693bba00af49fc9322d4e6ef2d6e022f8b3da
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-17 21:24:46 +0500

    services fixed

--- orders/services.py
+++ orders/services.py
@@ -5,14 +5,9 @@ class PaymentService:
 
     @staticmethod
     def process_payment(order, data=None):
-        """
-        Mock payment logic
-        """
 
-        # OPTION 1: Random success
         success = random.random() < 0.7
 
-        # OPTION 2 (optional): card-based simulation
         if data and data.get("card_number"):
             if data["card_number"] == "4242424242424242":
                 success = True
