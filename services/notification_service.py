9ee747b45821650fb599f61496d60417addb72a5
Jemsit0300 <jemsitbahadurow2@gmail.com>
2026-03-18 10:22:23 +0500

    fixed

---
+++ services/notification_service.py
@@ -0,0 +1,25 @@
+from .models import Notification
+
+
+class NotificationService:
+
+    @staticmethod
+    def notify_vendors(order):
+
+        notified_vendors = set()
+
+        for item in order.items.all():
+
+            vendor = item.product.vendor
+
+            # Prevent duplicate notifications
+            if vendor.id in notified_vendors:
+                continue
+
+            Notification.objects.create(
+                user=vendor,
+                type="order",
+                message=f"You received a new order #{order.id}"
+            )
+
+            notified_vendors.add(vendor.id)
